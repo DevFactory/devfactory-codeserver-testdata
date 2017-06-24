4ca6c5f919d2227630f91d578f018e8352def290
CodeServer Monitoring <codeserver-monitoring@aurea.com>
2017-06-24 09:47:23 +0000

    Modified dummy file at 2017/06/24-09:47:12.550

--- src/main/java/com/devfactory/codeserver/monitor/salarycalculator/Dummy.java
+++ src/main/java/com/devfactory/codeserver/monitor/salarycalculator/Dummy.java
@@ -2,6 +2,6 @@ package com.devfactory.codeserver.monitor.salarycalculator;
 
  class Dummy {
  
-    //dummy content modified at 2017/06/24-09:39:13.457
+    //dummy content modified at 2017/06/24-09:47:12.550
  
  }
