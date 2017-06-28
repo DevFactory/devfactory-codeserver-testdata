21182c78e8165dcf1c99d18b6448776b0686be53
CodeServer Monitoring <codeserver-monitoring@aurea.com>
2017-06-28 21:24:26 +0000

    Modified dummy file at 2017/06/28-21:24:24.794

--- src/main/java/com/devfactory/codeserver/monitor/salarycalculator/Dummy.java
+++ src/main/java/com/devfactory/codeserver/monitor/salarycalculator/Dummy.java
@@ -3,9 +3,9 @@ package com.devfactory.codeserver.monitor.salarycalculator;
  class Dummy {
  
 <<<<<<< HEAD
-    //dummy content modified at 2017/06/28-21:19:18.681
+    //dummy content modified at 2017/06/28-21:24:24.794
 =======
-    //dummy content modified at 2017/06/28-21:19:18.681
+    //dummy content modified at 2017/06/28-21:24:24.794
 >>>>>>> branch 'test-stg' of https://github.com/trilogy-group/devfactory-codeserver-testdata.git
  
  }
